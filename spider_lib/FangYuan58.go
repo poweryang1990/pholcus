0e07ccc7ecd0d5b85f31e059f007297fe26b4698
zhiping <zhiping@uoko.com>
2016-04-11 16:37:59 +0800

    reg used for spider

--- spider_lib/FangYuan58.go
+++ spider_lib/FangYuan58.go
@@ -17,7 +17,7 @@ import (
 	// "encoding/json"
 
 	// 字符串处理包
-    //"regexp"
+    "regexp"
 	"strconv"
 	"strings"
 	// 其他包
@@ -33,10 +33,13 @@ func init() {
 var FangYuan58 = &Spider{
 	Name:        "成都58房源抓取",
 	Description: "**成都58房源抓取**",
-	// Pausetime: 300,
+	Pausetime: 300,
 	// Keyin:   KEYIN,
 	// Limit:        LIMIT,
 	EnableCookie: false,
+    SubNamespace: func(self *Spider, dataCell map[string]interface{}) string {
+		return "cd_housesource_58"
+	},
 	RuleTree: &RuleTree{
 		Root: func(ctx *Context) {
 			ctx.AddQueue(&request.Request{
@@ -86,13 +89,16 @@ var FangYuan58 = &Spider{
 			"输出结果": {
 				//注意：有无字段语义和是否输出数据必须保持一致
 				ItemFields: []string{
-					"标题", "区域","商圈","小区", "地址","出租类型","房屋类型","房间大小","户型","租金","配置","装修","更新时间","楼层","经纪人","联系电话","链家发布","单价",
+					"标题", "区域","商圈","小区", "地址","出租类型","房屋类型","房间大小","户型","租金","配置","装修","更新时间","楼层","经纪人","联系电话","链家发布","单价","卧室",
 				},
 				ParseFunc: func(ctx *Context) {
+                    
 					query := ctx.GetDom()
 					var 标题, 区域,商圈,小区, 地址,出租类型,房屋类型,房间大小,户型,租金,配置,装修,更新时间,楼层,经纪人,联系电话,链家发布 string
+                    var 单价 int
                     标题 = query.Find(".house-title h1").Text()
-                    //digitsRegexp:= regexp.MustCompile("*.58.com/(.*?)x.shtml")
+                    
+                    租金 = query.Find("em.house-price").Text()  
                     
                     区域=query.Find("div.xiaoqu a").Eq(0).Text();
                     
@@ -115,29 +121,29 @@ var FangYuan58 = &Spider{
                     }else{
                          出租类型="合租"
                     }
-                  
-                    fangwuInfoList:= strings.Split(query.Find(".house-type").Text(),"层")
-                    fangwuInfoList1:= strings.Split(fangwuInfoList[0],"-")
-                    fangwuInfoList2:= strings.Split(fangwuInfoList[1],"-")
-                    if isZhengzu {
-                        房屋类型=strings.TrimSpace(fangwuInfoList2[2])
-                        房间大小=strings.TrimSpace(strings.Replace(fangwuInfoList1[1],"m²","",-1))
-                        户型=strings.TrimSpace(fangwuInfoList1[0])
-                        装修=strings.TrimSpace(fangwuInfoList2[0])
-                        楼层=strings.TrimSpace(fangwuInfoList[2])
-                    }else{
-                        房屋类型=strings.TrimSpace(fangwuInfoList2[2])
-                        房间大小=strings.TrimSpace(strings.Replace(fangwuInfoList1[2],"m²","",-1))
-                        户型=strings.TrimSpace(fangwuInfoList1[1])
-                        装修=strings.TrimSpace(fangwuInfoList2[0])
-                        楼层=strings.TrimSpace(fangwuInfoList1[2])
-                    }
-                   
-					租金 = query.Find("em.house-price").Text()  
+                    houseInfoText:=query.Find(".house-type").Text()
+                    houseTypeReg:=regexp.MustCompile("([\u4E00-\u9FA5]{0,2}住宅)|公寓|别墅|商住楼|商住两用|其他|(平房/四合院)")
+                    houseSizeReg:=regexp.MustCompile("(\\d+)(\\s+)m²")
+                    houseHuXinReg:=regexp.MustCompile("(\\d+)室(\\s+)(\\d+)厅(\\s+)(\\d+)卫")
+                    houseZhuangXiuReg:=regexp.MustCompile("[\u4E00-\u9FA5]{1,2}装修")
+                    houseFloorReg:=regexp.MustCompile("(\\d+)/(\\d+)层")
+                    houseRoomReg:=regexp.MustCompile("[\u4E00-\u9FA5]{1,}卧")
+                    房屋类型=houseTypeReg.FindString(houseInfoText)
+                    房间大小=strings.Replace(houseSizeReg.FindString(houseInfoText),"m²","",-1)
+                    户型=houseHuXinReg.FindString(houseInfoText)
+                    装修=houseZhuangXiuReg.FindString(houseInfoText)
+                    楼层=houseFloorReg.FindString(houseInfoText)
+                    
+                    卧室:=houseRoomReg.FindString(houseInfoText)
+                    
+					
                    
                     price,_:= strconv.Atoi(租金)   
                     size,_:=strconv.Atoi(房间大小)
-                    单价:= price/size 
+                    if size!=0 {
+                      单价= price/size  
+                    }
+                    
                        
                     query.Find("ul.house-primary-content>li").Last().Find("div>span").Each(func (i int, s *goquery.Selection)  {
                         if i==0 {
@@ -159,7 +165,7 @@ var FangYuan58 = &Spider{
 
 					// 结果输出方式一（推荐）
 					ctx.Output(map[int]interface{}{
-						0:标题,1:区域,2:商圈,3:小区,4: 地址,5:出租类型,6:房屋类型,7:房间大小,8:户型,9:租金,10:配置,11:装修,12:更新时间,13:楼层,14:经纪人,15:联系电话,16:链家发布,17:单价,
+						0:标题,1:区域,2:商圈,3:小区,4: 地址,5:出租类型,6:房屋类型,7:房间大小,8:户型,9:租金,10:配置,11:装修,12:更新时间,13:楼层,14:经纪人,15:联系电话,16:链家发布,17:单价,18:卧室,
 					})
 
 					
